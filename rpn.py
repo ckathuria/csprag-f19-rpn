a3d0f73233af241b26352d08190cc416ea35a5e6
carissak <carissak@umich.edu>
2019-11-05 11:55:12 -0500

    Adding code for exponential

--- rpn.py
+++ rpn.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import readline
+from termcolor import colored
+
 def calculate(arg):
     stack = list()
     for token in arg.split():
@@ -13,12 +16,21 @@ def calculate(arg):
                 arg2 = stack.pop()
                 result = arg2 - arg1
                 stack.append(result)
+        elif token == '^':
+            arg1 = stack.pop()
+            arg2 = stack.pop()
+            result = arg2 ** arg1
+            stack.append(result)
         else:
             stack.append(int(token))
         # print(stack)
     if len(stack) != 1:
         raise TypeError("Malformed input")
     answer = stack.pop()
+    if (answer < 0):
+        print(colored(answer, 'red'))
+    #else:
+        #print(colored(answer, 'black'))
     print(answer)
     return answer
 
